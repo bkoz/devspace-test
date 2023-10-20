4b8742680dabb7d6b91818c0addc20ccddcf28fb
Bob Kozdemba <bkozdemba@gmail.com>
2023-10-20 21:00:08 +0000

    Added extensions

--- 01-code-example.py
+++ 01-code-example.py
@@ -1,3 +1,22 @@
 #
 # Type a few code hints and choose Shift-command-L to get help from the Code Llama!
 #
+def fibonacci(n:int) -> int:
+    """
+    This function returns the nth Fibonacci number.
+    """
+    if n < 0:
+        raise ValueError("Fibonacci numbers are only defined for non-negative integers.")
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+def main():
+    print(fibonacci(10))
+
+if __name__ == "__main__":
+    main()
